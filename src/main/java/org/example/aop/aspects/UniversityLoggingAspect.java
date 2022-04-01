8e824228545bdf06cb559a75c52fbd8471625e7e
Anna Fabrichnaya <fabrichnaya.anna@bk.ru>
2022-04-01 21:24:09 +0300

    afterThrowing advice

--- src/main/java/org/example/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/org/example/aop/aspects/UniversityLoggingAspect.java
@@ -1,9 +1,6 @@
 package org.example.aop.aspects;
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.example.aop.Student;
 import org.springframework.stereotype.Component;
 
@@ -18,24 +15,32 @@ public class UniversityLoggingAspect {
                 "списка студентов перед методом getStudents");
     }
 
-    @AfterReturning(pointcut = "execution(* getStudents())"
-    , returning = "students")
-    public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {
+    @AfterThrowing(pointcut = "execution(* getStudents())"
+    , throwing = "exception")
+    // получаем информацию об эксепшине. который выбрасывается
+    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception) {
+        System.out.println("afterThrowingGetStudentsLoggingAdvice: логируем выброс " +
+                "исключения" + exception);
+    }
+
+ //   @AfterReturning(pointcut = "execution(* getStudents())"
+ //   , returning = "students")
+ //   public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {
         //получаем информацию о первом студенте и меняем ему имя.Эдвайс выполняется
         // до выдачи нам конечного результата
         // в консоль, т.е. мы получаем уже обновленный результат в консоль.
         //Изменяется возвращаемый результат метода.
         //Важно комментировать такой код
-        Student firstStudent = students.get(0);
-        String nameSurname = firstStudent.getNameSurname();
-        nameSurname = "Mr." + nameSurname;
-        firstStudent.setNameSurname(nameSurname);
-        double avgGrade = firstStudent.getAvgGrade();
-        avgGrade = avgGrade + 1;
-        firstStudent.setAvgGrade(avgGrade);
-        System.out.println("afterReturningGetStudentsLoggingAdvice: логируем получение +" +
-                "списка студентов после работы метода getStudents");
-    }
+//        Student firstStudent = students.get(0);
+//        String nameSurname = firstStudent.getNameSurname();
+//        nameSurname = "Mr." + nameSurname;
+//        firstStudent.setNameSurname(nameSurname);
+//        double avgGrade = firstStudent.getAvgGrade();
+//        avgGrade = avgGrade + 1;
+//        firstStudent.setAvgGrade(avgGrade);
+//        System.out.println("afterReturningGetStudentsLoggingAdvice: логируем получение +" +
+//                "списка студентов после работы метода getStudents");
+//    }
 
 
 }
